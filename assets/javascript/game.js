94583c08b2f8dcd0cc1abf39d8ef7bb907c04f82
Ash <asha.mohamed.28@gmail.com>
2018-06-10 19:18:14 -0400

    HTML and CSS complete. initial functionality started

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$("document").ready(function(){
+
+// =====================================
+// MAJOR TASK 1: Generate Random Number
+// =====================================
+
+var randomNum = Math.floor(Math.random() * 100); 
+
+$("#randomNum").text(randomNum); 
+
+
+
+
+
+
+
+
+
+
+
+
+}); 
