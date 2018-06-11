782c9669c15a61e5cfaf4f7a455a91fcc7abe60b
Ash <asha.mohamed.28@gmail.com>
2018-06-10 20:21:29 -0400

    random number generated for computer target and gem buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,34 @@ $("document").ready(function(){
 // MAJOR TASK 1: Generate Random Number
 // =====================================
 
-var randomNum = Math.floor(Math.random() * 100); 
+var randomNumComp = Math.floor(Math.random() * (120-19)+19); 
 
-$("#randomNum").text(randomNum); 
-
-
+$("#randomNum").text(randomNumComp); 
 
 
+// =====================================
+// MAJOR TASK 2: Generate Random Number
+// =====================================
 
+var btnNum = function() {
+    var randomNumGem1 = Math.floor(Math.random() * (12-1) + 1 );  
+    $("#gemBtn1").text(randomNumGem1);     
 
+    var randomNumGem2 = Math.floor(Math.random() * (12-1) + 1 );  
+    $("#gemBtn2").text(randomNumGem2);     
+
+    var randomNumGem3 = Math.floor(Math.random() * (12-1) + 1 );  
+    $("#gemBtn3").text(randomNumGem3);     
+
+    var randomNumGem4 = Math.floor(Math.random() * (12-1) + 1 );  
+    $("#gemBtn4").text(randomNumGem4);     
+}
+
+btnNum(); 
+
+// =====================================================
+// MAJOR TASK 3: Button Click Gets Added to Player Score
+// =====================================================
 
 
 
