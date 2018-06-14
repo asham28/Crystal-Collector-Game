41059df59a6ced0c5801e9828d701a8ef5d0e6b7
Ash <asha.mohamed.28@gmail.com>
2018-06-14 13:05:05 -0400

    functionality complete css improvements made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,130 @@
-$("document").ready(function(){
+$("document").ready(function () {
 
-// =====================================
-// MAJOR TASK 1: Generate Random Number
-// =====================================
+    // ==============================================================
+    // MAJOR TASK: BUTTON CLICK FOR RULES
+    // ==============================================================
 
-var randomNumComp = Math.floor(Math.random() * (120-19)+19); 
+// Rules are hidden on start up
+$("#p-background").show();
 
-$("#randomNum").text(randomNumComp); 
-
-
-// =====================================
-// MAJOR TASK 2: Generate Random Number
-// =====================================
-
-var btnNum = function() {
-    var randomNumGem1 = Math.floor(Math.random() * (12-1) + 1 );  
-    $("#gemBtn1").text(randomNumGem1);     
-
-    var randomNumGem2 = Math.floor(Math.random() * (12-1) + 1 );  
-    $("#gemBtn2").text(randomNumGem2);     
-
-    var randomNumGem3 = Math.floor(Math.random() * (12-1) + 1 );  
-    $("#gemBtn3").text(randomNumGem3);     
-
-    var randomNumGem4 = Math.floor(Math.random() * (12-1) + 1 );  
-    $("#gemBtn4").text(randomNumGem4);     
-}
-
-btnNum(); 
-
-// =====================================================
-// MAJOR TASK 3: Button Click Gets Added to Player Score
-// =====================================================
+    $("#hide").click(function () {
+        $("#p-background").hide();
+    });
+    $("#show").click(function () {
+        $("#p-background").show();
+    });
 
 
 
+    // ==============================================================
+    // MAJOR TASK: Set variables 
+    // ==============================================================
+
+    var wins = 0;
+    var losses = 0;
+    var previous = 0; //change to counter later
+    var targetNumber;
+
+    // put images of crystals into an array
+    var images = [
+        "assets/images/gem1.jpg",
+        "assets/images/gem2.png",
+        "assets/images/gem3.png",
+        "assets/images/gem4.png"
+    ]
+
+
+    // // ==============================================================
+    // // MAJOR TASK: START AND RESET CONDITIONS
+    // // ==============================================================
+    var startGame = function () {
+
+        targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+        $("#targetNum").html(targetNumber);
+
+        console.log("Target : " + targetNumber)
+
+        $(".crystals").empty();
+
+        // // First, create the random number between 1-12 for each crystal
+        for (var i = 0; i < 4; i++) {
+            var randomNumber = Math.floor(Math.random() * (12 - 1) + 1);
+            console.log(randomNumber);
+
+            var crystal = $("<div>");
+            crystal.attr({
+                "class": 'crystal',
+                "data-randomNumber": randomNumber,
+            });
+
+            // append image of crystal to each div 
+            crystal.css({
+                "background-image": "url('" + images[i] + "')",
+                "background-size": "cover"
+            });
+
+            $(".crystals").append(crystal);
+
+        }
+    }
 
 
 
-}); 
+    // user clicks crystal and numbers add together CLICKING THE CRYSTALS
+    $(document).on('click', ".crystal", function () {
+
+
+        var userNum = parseInt($(this).attr('data-randomNumber'));
+
+        previous += userNum
+
+        $("#score-num").html(previous);
+
+
+
+        // // ==============================================================
+        // // MAJOR TASK: LOGIC CONDITIONS
+        // // ==============================================================
+
+        // // LOSING SCENARIO
+        // // ================
+        if (previous > targetNumber) {
+            console.log("You lost!");
+            alert("You lose! Your final total score was: " + previous);
+            // increase losses by 1
+            losses++;
+            // update HTML element
+            $("#losses").html("Losses: " + losses);
+            // reset score
+            previous = 0;
+            // reset game
+            startGame();
+
+            // //WINING SCENARIO
+            // //=================
+        } else if (previous === targetNumber) {
+            console.log("You win!!");
+            alert("You win!");
+            //increase wins by 1
+            wins++;
+            // update HTML element
+            $("#wins").html("Wins " + wins);
+            // reset score  
+            previous = 0;
+            // reset game
+            startGame();
+        }
+
+
+    });
+
+
+
+    // // =========================================================
+    // // CALL GAME TO START WHEN PAGE LOADS
+    // // =========================================================
+
+    startGame();
+
+
+});
